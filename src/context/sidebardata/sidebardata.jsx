787a13686e58a06066dcac5dab02c244333d4741
ALOK9442 <alok2110036@akgec.ac.in>
2024-12-19 22:44:54 +0530

    project initialisation

---
+++ src/context/sidebardata/sidebardata.jsx
@@ -0,0 +1,7 @@
+export const navigationLinks = [
+  { id: 1, label: "Repositories", route: "/repositories" },
+  { id: 2, label: "AI Code Review", route: "/ai-code-review" },
+  { id: 3, label: "Cloud Security", route: "/cloud-security" },
+  { id: 4, label: "How to Use", route: "/how-to-use" },
+  { id: 5, label: "Settings", route: "/settings" },
+];
